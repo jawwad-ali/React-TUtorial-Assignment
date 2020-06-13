27b21005e04d0ef530e345d336cc568f640b10d1
jawwad-ali <alijawwad001@gmail.com>
2020-06-13 12:40:14 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    let [isLit, setLit] = useState("On")
+    let [Temp , SetTemp] = useState(22);
+
+    const Inc = () => {
+        SetTemp (Temp + 1)
+    }
+    const Dec = () => {
+        SetTemp (Temp - 1)
+    }
+
+    return (
+        <div className={`room ${isLit}`}>
+            <p> The lights are {isLit} </p>
+            <button className="on" onClick={() => { setLit(isLit = "ON") }}>
+                Turn ON
+            </button>
+
+            &nbsp;
+            
+            <button className="off" onClick={() => { setLit(isLit = "OFF") }}>
+                Turn OFF
+            </button>
+
+            <h2> Temperature </h2>
+            <p> Initial Temperature: {Temp} </p>  
+            <button onClick={Inc}>+ </button> &nbsp;
+            <button onClick={Dec}>- </button> 
+
+        </div>
+
+    )
 }
-
-export default App;
